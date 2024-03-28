8ff6b0f5cd3f25e72a407cafdbfadc2c64cb82eb
lukevinicius <lucasviniciusaalves@gmail>
2024-03-28 11:35:42 -0300

    remove: google adsense tags

--- src/app/scripts.tsx
+++ src/app/scripts.tsx
@@ -4,12 +4,12 @@ export function ScriptApp() {
   return (
     <>
       <head>
-        <meta name="google-adsense-account" content="ca-pub-9171417617926228" />
+        {/* <meta name="google-adsense-account" content="ca-pub-9171417617926228" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9171417617926228"
           crossOrigin="anonymous"
-        />
+        /> */}
       </head>
       <GoogleTagManager gtmId="GTM-PN2MJNS5" />
       <GoogleAnalytics gaId="G-PFVTVSCZQC" />
