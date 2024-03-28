077c52e380534da85987ba9575b060bc5294f9e5
lukevinicius <lucasviniciusaalves@gmail>
2024-03-28 09:33:57 -0300

    feat: google adsense

--- src/app/scripts.tsx
+++ src/app/scripts.tsx
@@ -3,6 +3,9 @@ import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 export function ScriptApp() {
   return (
     <>
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-9171417617926228" />
+      </head>
       <GoogleTagManager gtmId="GTM-PN2MJNS5" />
       <GoogleAnalytics gaId="G-PFVTVSCZQC" />
     </>
